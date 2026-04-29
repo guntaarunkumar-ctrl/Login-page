6897358c9636d63fd7bfc17e7be0d75d2739821c
guntaarunkumar-ctrl <guntaarunkumar@gmail.com>
2026-04-29 19:25:21 +0530

    Fix firebase-config.js to use compat CDN syntax

--- firebase-config.js
+++ firebase-config.js
@@ -1,21 +1,10 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyD_hfvqE7aungNRLzNZpQzARLkwQML7dGo",
-  authDomain: "realtimeplanners-appv3.firebaseapp.com",
-  projectId: "realtimeplanners-appv3",
-  storageBucket: "realtimeplanners-appv3.firebasestorage.app",
+  apiKey:            "AIzaSyD_hfvqE7aungNRLzNZpQzARLkwQML7dGo",
+  authDomain:        "realtimeplanners-appv3.firebaseapp.com",
+  projectId:         "realtimeplanners-appv3",
+  storageBucket:     "realtimeplanners-appv3.firebasestorage.app",
   messagingSenderId: "61594828338",
-  appId: "1:61594828338:web:a1837ce15f7c30aa0d462b",
-  measurementId: "G-0GWY4PDN7M"
+  appId:             "1:61594828338:web:a1837ce15f7c30aa0d462b"
 };
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+firebase.initializeApp(firebaseConfig);
